aaa13155633a3c3a21985a7c233ee3557abc9ab1
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-08-30 21:09:26 +0000

    Import redirect from Flask

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template,
-    request, session, url_for)
+    Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
