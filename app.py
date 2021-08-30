0df1b8734855ef08c9d8efe3f39dc4e360dd7dac
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-08-30 17:25:10 +0000

    Create library route and view

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+@app.route("/")
+@app.route("/library")
+def library():
+    books = list(mongo.db.books.find())
+    return render_template("library.html", books=books)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
