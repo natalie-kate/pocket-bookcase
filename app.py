8c0f6c35449abfe625d672c43adf8e2c40607915
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-09-29 22:02:41 +0000

    Started commenting code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# imports
 import os
 from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
@@ -7,65 +8,77 @@ from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
+# configurations
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-
 mongo = PyMongo(app)
 
 
+# library view
 @app.route("/")
 @app.route("/library")
 def library():
+    # finds books in db to pass into template
     books = list(mongo.db.books.find())
+    # If a user is logged in then admin will be passed
+    # into template as well.
     if session:
         admin = mongo.db.users.find_one(
-          {"username": session["user"]})["admin"]
+            {"username": session["user"]})["admin"]
         return render_template("library.html", books=books, admin=admin)
-
     return render_template("library.html", books=books)
 
 
+# search_library view, gets user input from search box and looks
+# for a match in the db.
 @app.route("/search_library", methods=["GET", "POST"])
 def search_library():
     if request.method == "POST":
         search = request.form.get("search").lower()
         results = list(mongo.db.books.find(
             {"$text": {"$search": search}}))
+        # If there are results then check whether user logged in
+        # to determine whether admin needs passed in or not.
         if results:
-            if session["user"]:
+            if session:
                 admin = mongo.db.users.find_one(
-                  {"username": session["user"]})["admin"]
+                    {"username": session["user"]})["admin"]
                 return render_template(
                     "library.html", admin=admin, results=results)
-            return render_template(
-              "library.html", results=results)
+            else:
+                return render_template(
+                    "library.html", results=results)
+        # If there are no results then display flash message.
         else:
             flash(f"Sorry cannot find { search.title()}")
             return redirect(url_for("library"))
 
 
+# Register view
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    # Get genres from db to pass into template
     genres = list(mongo.db.genres.find())
+    # If registration form submitted check whether username already taken
     if request.method == "POST":
         existing = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()}
-        )
+            {"username": request.form.get("username").lower()})
+        # If username taken display flash message and reload page.
         if existing:
             flash(
                 "Username is taken, try adding numbers or choose",
                 "another username"
                 )
             return redirect(url_for("register"))
-
+        # If password and confirmation password don't match
+        # display flash message and reload page.
         if request.form.get("password") != request.form.get(
                 "confirm-password"):
             flash("Your passwords did not match, please try again")
             return redirect(url_for("register"))
-
+        # Get information for new user from registration form
         register_user = {
             "first_name": request.form.get("first_name"),
             "surname": request.form.get("surname"),
@@ -75,23 +88,24 @@ def register():
             "genre": request.form.get("genre"),
             "admin": bool("")
         }
+        # New user created in db
         mongo.db.users.insert_one(register_user)
-
-        # put the new user into 'session' cookie
+        # Put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
+        # Create users profile document in db
         user_id = mongo.db.users.find_one(
             {"username": session["user"]})["_id"]
-        name = request.form.get("first_name")
         mongo.db.profiles.insert_one({
             "user_id": ObjectId(user_id),
             "books_to_read": [],
             "own_books": [],
             "read_books": []})
+        # Display personalised message and load profile page.
+        name = request.form.get("first_name")
         flash(
             f'Welcome {name} ' +
             'you have been successfully registered.')
         return redirect(url_for("profile", username=session["user"]))
-
     return render_template("register.html", genres=genres)
 
 
